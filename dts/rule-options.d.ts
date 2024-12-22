364ff804e177349634295537e7fe3b3f40b1deef
ntnyq <ntnyq13@gmail.com>
2024-12-23 05:47:11 +0800

    fix: exclude void from rule options deeply

--- dts/rule-options.d.ts
+++ dts/rule-options.d.ts
@@ -1,10 +1,28 @@
 import type { Linter } from 'eslint'
 import type { Config as SVGOOptions } from 'svgo'
 
+type DeepExcludeVoid<T> = T extends void
+  ? never
+  : T extends object
+    ? {
+        [K in keyof T]: DeepExcludeVoid<T[K]>
+      }
+    : T extends (infer U)[]
+      ? DeepExcludeVoid<U>[]
+      : T extends (...args: any[]) => any
+        ? (...args: Parameters<T>) => DeepExcludeVoid<ReturnType<T>>
+        : Exclude<T, void>
+
+/**
+ * rule options
+ */
 export type RuleOptions = {
-  'svgo/svgo': [SVGOOptions]
+  'svgo/svgo': [DeepExcludeVoid<SVGOOptions>]
 }
 
+/**
+ * rules
+ */
 export type Rules = {
   [K in keyof RuleOptions]: Linter.RuleEntry<RuleOptions[K]>
 }
