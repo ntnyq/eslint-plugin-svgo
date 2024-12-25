203e1edf4f4e496f1fa026bc12e0182bad6e0527
ntnyq <ntnyq13@gmail.com>
2024-12-26 02:45:04 +0800

    test: add test cases for js2svg

--- tests/rules/svgo.test.ts
+++ tests/rules/svgo.test.ts
@@ -6,6 +6,129 @@ run({
   name: 'svgo',
   rule,
   invalid: [
+    /**
+     * ===========================================
+     *                 js2svg
+     * ===========================================
+     */
+    {
+      description: 'js2svg default',
+      filename: 'file.svg',
+      options: {
+        plugins: [],
+      },
+      code: $`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!-- Created with love! -->
+          <defs>
+            <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+          </defs>
+          <g>
+            <use href="#circle" transform="skewX(16)"/>
+            <rect id="useless" width="0" height="0" fill="#ff0000"/>
+          </g>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(`
+          "<?xml version="1.0" encoding="UTF-8"?>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!--Created with love!-->
+            <defs>
+              <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+            </defs>
+            <g>
+              <use href="#circle" transform="skewX(16)"/>
+              <rect id="useless" width="0" height="0" fill="#ff0000"/>
+            </g>
+          </svg>
+          "
+        `)
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+    {
+      description: 'js2svg pretty false',
+      filename: 'file.svg',
+      options: {
+        js2svg: {
+          pretty: false,
+        },
+        plugins: [],
+      },
+      code: $`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!-- Created with love! -->
+          <defs>
+            <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+          </defs>
+          <g>
+            <use href="#circle" transform="skewX(16)"/>
+            <rect id="useless" width="0" height="0" fill="#ff0000"/>
+          </g>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(
+          `"<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150"><!--Created with love!--><defs><ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/></defs><g><use href="#circle" transform="skewX(16)"/><rect id="useless" width="0" height="0" fill="#ff0000"/></g></svg>"`,
+        )
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+    {
+      description: 'js2svg indent 4',
+      filename: 'file.svg',
+      options: {
+        js2svg: {
+          indent: 4,
+        },
+        plugins: [],
+      },
+      code: $`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!-- Created with love! -->
+          <defs>
+            <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+          </defs>
+          <g>
+            <use href="#circle" transform="skewX(16)"/>
+            <rect id="useless" width="0" height="0" fill="#ff0000"/>
+          </g>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(`
+          "<?xml version="1.0" encoding="UTF-8"?>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!--Created with love!-->
+              <defs>
+                  <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+              </defs>
+              <g>
+                  <use href="#circle" transform="skewX(16)"/>
+                  <rect id="useless" width="0" height="0" fill="#ff0000"/>
+              </g>
+          </svg>
+          "
+        `)
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+
+    /**
+     * ===========================================
+     *                 preset-default
+     * ===========================================
+     */
     {
       description: 'default preset-default',
       filename: 'file.svg',
@@ -81,7 +204,59 @@ run({
       },
     },
     {
-      description: 'plugin cleanupIds',
+      description: 'preset-default with cleanupIds params',
+      filename: 'file.svg',
+      options: {
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupIds: {
+                  // minify referenced IDs
+                  minify: false,
+                },
+              },
+            },
+          },
+        ],
+      },
+      code: $`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox=" 0 0  150 100 " width="150">
+          <!-- Created with love! -->
+          <defs>
+            <ellipse cx="50" cy="50.0" rx="50.00" ry="auto" fill="black" id="circle"/>
+          </defs>
+          <g>
+            <use href="#circle" transform="skewX(16)"/>
+            <rect id="useless" width="0" height="0" fill="#ff0000"/>
+          </g>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(`
+          "<svg xmlns="http://www.w3.org/2000/svg" width="150" viewBox="0 0 150 100">
+            <defs>
+              <circle id="circle" cx="50" cy="50" r="50" fill="#000"/>
+            </defs>
+            <use href="#circle" transform="skewX(16)"/>
+          </svg>
+          "
+        `)
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+
+    /**
+     * ===========================================
+     *                 plugins
+     * ===========================================
+     */
+    {
+      description: 'plugin cleanupIds name',
       filename: 'file.svg',
       options: {
         plugins: ['cleanupIds'],
@@ -95,6 +270,63 @@ run({
         expect(output).toMatchInlineSnapshot(`
           "<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 100" width="150">
             <rect width="0" height="0" fill="#ff0000"/>
+          </svg>
+          "
+        `)
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+    {
+      description: 'plugin cleanupIds object',
+      filename: 'file.svg',
+      options: {
+        plugins: [
+          {
+            name: 'cleanupIds',
+          },
+        ],
+      },
+      code: $`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 100" width="150">
+          <rect id="useless" width="0" height="0" fill="#ff0000"/>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(`
+          "<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 100" width="150">
+            <rect width="0" height="0" fill="#ff0000"/>
+          </svg>
+          "
+        `)
+      },
+      errors(errors) {
+        expect(errors).toMatchSnapshot()
+      },
+    },
+    {
+      description: 'plugin cleanupIds with params',
+      filename: 'file.svg',
+      options: {
+        plugins: [
+          {
+            name: 'cleanupIds',
+            params: {
+              remove: false,
+            },
+          },
+        ],
+      },
+      code: $`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 100" width="150">
+          <rect id="useless" width="0" height="0" fill="#ff0000"/>
+        </svg>
+      `,
+      output(output) {
+        expect(output).toMatchInlineSnapshot(`
+          "<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 100" width="150">
+            <rect id="useless" width="0" height="0" fill="#ff0000"/>
           </svg>
           "
         `)
