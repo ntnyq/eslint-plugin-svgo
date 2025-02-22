a3cc737d4e7e265129940b9c1999f46a0c390c72
ntnyq <ntnyq13@gmail.com>
2025-02-23 01:30:54 +0800

    ci: setup autofix action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,4 @@ import { defineESLintConfig } from '@ntnyq/eslint-config'
 export default defineESLintConfig({
   ignores: ['**/dts/rule-options.d.ts'],
   svgo: true,
-  ntnyq: {
-    overrides: {
-      'ntnyq/no-duplicate-exports': 'error',
-    },
-  },
 })
